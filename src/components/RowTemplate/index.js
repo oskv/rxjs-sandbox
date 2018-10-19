2be89075ab59c14b029739eca33175172b0c80ef
Alexandr Skakovskiy <a.skakovsky@viseven.com>
2018-10-19 14:43:22 +0300

    row styles

--- src/components/RowTemplate/index.js
+++ src/components/RowTemplate/index.js
@@ -1,5 +1,7 @@
 import React, {PureComponent} from 'react';
+import './styles.css'
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 export default class RowTemplate extends PureComponent {
   constructor(props) {
@@ -13,11 +15,15 @@ export default class RowTemplate extends PureComponent {
 
   render() {
     return (
-      <Grid  container className='row-template'>
-      { this.columns.map((column, i) =>
-          <Grid key={i} item xs={this.getWidth(column.width)}>col</Grid>
-      )}
-      </Grid>
+      <Paper className="row-template">
+        <Grid  container className='grid' justify="center">
+        { this.columns.map((column, i) =>
+            <Grid key={i} item xs={this.getWidth(column.width)}>
+              <span className="col"/>
+            </Grid>
+        )}
+        </Grid>
+      </Paper>
     );
   }
 }
--- src/components/RowTemplatesList/index.js
+++ src/components/RowTemplatesList/index.js
@@ -1,31 +1,30 @@
 import React, {PureComponent} from 'react';
 import RowTemplate from '../RowTemplate';
+import './styles.css'
 
 export default class RowTemplatesList extends PureComponent {
   constructor(props) {
     super(props);
     this.rows = [
-      { id: 'row_100', columns: [{ width: 100 }]},
-      { id: 'row_2x50', columns: [{ width: 50 }, { width: 50 }]},
-      { id: 'row_65_35', columns: [{ width: 65 }, { width: 35 }]},
-      { id: 'row_35_65', columns: [{ width: 35 }, { width: 65 }]},
-      { id: 'row_3x33', columns: [{ width: 33 }, { width: 33 }, { width: 33 }]},
-      { id: 'row_4x25', columns: [{ width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }]},
+      { columns: [{ width: 100 }]},
+      { columns: [{ width: 50 }, { width: 50 }]},
+      { columns: [{ width: 60 }, { width: 40 }]},
+      { columns: [{ width: 40 }, { width: 60 }]},
+      { columns: [{ width: 33 }, { width: 33 }, { width: 33 }]},
+      { columns: [{ width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }]},
     ];
   }
 
   render() {
-    const listRows = this.rows.map((row) =>
-      <li key={row.id}>
+    const listRows = this.rows.map((row, i) =>
+      <li key={i}>
         <RowTemplate columns={row.columns} />
       </li>
     );
     return (
-      <div className='row-templates-list'>
-        <ul>
-          {listRows}
-        </ul>
-      </div>
+      <ul className='row-templates-list'>
+        {listRows}
+      </ul>
     );
   }
 }
