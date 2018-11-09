9a7188b18329c3f5b984d8b23c80530c86e783df
Alexandr Skakovskiy <a.skakovsky@viseven.com>
2018-11-09 10:40:32 +0200

    update: block templates

--- src/components/BlockTemplate/index.js
+++ src/components/BlockTemplate/index.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { DragSource } from 'react-dnd';
+import Icon from '@material-ui/core/Icon';
 
 const boxSource = {
   beginDrag(props) {
@@ -26,6 +27,7 @@ class BlockTemplate extends PureComponent {
   constructor(props) {
     super(props);
     this.type = props.type;
+    this.icon = props.icon;
   }
 
   render() {
@@ -33,7 +35,8 @@ class BlockTemplate extends PureComponent {
     return connectDragSource && connectDragSource(
       <div className="block-template">
         <Card>
-          <CardContent>
+          <CardContent className='content'>
+            <Icon>{this.icon}</Icon>
             <Typography variant="subheading">{this.type}</Typography>
           </CardContent>
         </Card>
--- src/components/BlockTemplatesList/index.js
+++ src/components/BlockTemplatesList/index.js
@@ -7,9 +7,11 @@ export default class RowTemplatesList extends PureComponent {
   constructor(props) {
     super(props);
     this.blocks = [
-      { type: 'text', icon: '', data: { text: 'Input som text here'}},
-      { type: 'image', icon: ''},
-      { type: 'button', icon: ''},
+      { type: 'text', icon: 'text_format', data: { text: 'Input som text here'}},
+      { type: 'image', icon: 'image'},
+      { type: 'button', icon: 'insert_link'},
+      { type: 'divider', icon: 'vertical_align_center'},
+      { type: 'video', icon: 'videocam'},
     ];
   }
 
